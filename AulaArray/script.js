8b3f5add5a14db5520b4a2b9be93ba73b0467b44
Lucasvpl <lucasvpldeveloper@gmail.com>
2024-11-10 18:09:08 -0300

    second commit

---
+++ AulaArray/script.js
@@ -0,0 +1,37 @@
+console.log('Boa noite!!')
+
+// Array
+
+let nome = 'Vini'
+
+let frutas = ['Maça', 'banana','Laranja', 'Uva']
+
+console.log(frutas[0])
+
+
+
+let series = ['game of thrones', ' Todo mundo odeia o Cris']
+
+
+series.unshift('Adiciona no começo')
+
+
+series.push('Adiciona no final')
+
+
+series.shift()
+
+
+series.pop()
+
+
+
+let numeros = [2,5,9,3,1,4,6,8,7,20,50,100,66]
+
+numeros.sort()
+console.log(numeros)
+
+
+let novaSerie = series.slice(0,1)
+console.log(novaSerie)
+
---
+++ AulaJs/script.js
@@ -0,0 +1,32 @@
+console.log('Olá mundo!')
+console.log(3 * 5)
+
+
+// tipos de variaveis 
+
+// var aluno = 'lucas' <- não utilizamos mais a variavel do tipo (var)
+
+let nome = 'Lucas'
+
+nome = 'Marina'
+console.log(nome)
+
+const cpf = 88888888
+console.log(typeof cpf)
+
+console.log(typeof nome, cpf)
+
+let diaLindo = true
+
+console.log(diaLindo)
+
+console.log(typeof diaLindo)
+
+let data;
+data = 20
+console.log(data)
+
+console.log(5 * "y")
+
+let ola = null 
+
